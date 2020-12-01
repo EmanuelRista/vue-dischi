87fb4fcaee12f53fe6f357602bcc627da09a7ea4
Emanuel <e.rista@outlook.it>
2020-12-01 16:01:17 +0100

    Bonus completed

---
+++ script.js
@@ -0,0 +1,26 @@
+const app = new Vue({
+  el: "#myApp",
+  data: {
+    albums: new Array(),
+    selected: null,
+  },
+  computed: {
+    filteredCDs() {
+      if (this.selected == null) {
+        return this.albums
+      } else {
+        return this.albums.filter(album => album.genre == this.selected)
+      }
+    }
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((result) => {
+        this.albums = result.data.response;
+      })
+      .catch((error) => {
+        alert(`${error}`);
+      });
+  },
+});
